61bbffeb2b1ee031c37fa28e4a89aac5075c7469
Lina Zughaiyer <linazughaiyer@hotmail.com>
2021-02-16 16:36:24 -0500

    set up tables in databse

--- 02-Homework/index.js
+++ 02-Homework/index.js
@@ -1,1 +1,15 @@
-console.log("testing");
+const inquirer = require("inquirer");
+
+function init() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "initalOption",
+            message: "What would you like to do?",
+            choices: ["View all Employees", "View all Employees by department", "View all Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Add Role"]
+        }]);
+}
+
+init();
+
+
