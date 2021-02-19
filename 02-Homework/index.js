9dc8cb1ce97fb79466a00616fd99c5242a3cad12
Lina Zughaiyer <linazughaiyer@hotmail.com>
2021-02-19 13:52:15 -0500

    add role view option

--- 02-Homework/index.js
+++ 02-Homework/index.js
@@ -26,7 +26,7 @@ function init() {
             type: "list",
             name: "initalOption",
             message: "What would you like to do?",
-            choices: ["View all Employees", "View all Employees by department", "View all Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "Add Role"]
+            choices: ["View all Employees", "View all Employees by department", "View all Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View all Roles"]
         }]);
 }
 
@@ -35,17 +35,37 @@ init().then(answers => {
         showAllEmployees();
     }
     else if (answers.initalOption == "View all Employees by department") {
-
+        showAllEmployeesByDept();
     }
+
+
+    else if (answers.initalOption == "View all Roles") {
+        viewAllRoles();
+    }
+
 
 }
 );
 
+function viewAllRoles() {
+    let sql = "SELECT * FROM role";
+    let query = db.query(sql, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+    }
+    )
+
+
+}
+
+
+
+// try to make this similar but for roles
 function showAllEmployees() {
     let sql = "SELECT * FROM employees";
     let query = db.query(sql, (err, results) => {
         if (err) throw err;
-        console.log(results);
+        console.table(results);
     }
 
     );
@@ -54,41 +74,34 @@ function showAllEmployees() {
 //showAllEmployees();
 
 
-function viewAllEmployeesByDepartment(selectedDepartment) {
+function showAllEmployeesByDept() {
     inquirer.prompt([
         {
             type: "text",
-            name: "selectedDepartment",
-            message: "enter department name"
-
-
+            name: "selectedDep",
+            message: "Enter department name: "
         }
-
-
     ]).then(answers => {
-        let sql0 = `SELECT * FROM department WHERE name = "${answers.selectedDepartment}"`;
+
+        let sql0 = `SELECT * FROM department WHERE name = "${answers.selectedDep}"`;
         let query0 = db.query(sql0, (err, results0) => {
             if (err) throw err;
-            let department_id = results0[0].department_id;
-            let sql1 = `SELECT * FROM role WHERE department_id = ${department_id}`;
+            // console.log(results0);
+            let depId = results0[0].department_id;
+            // console.log(depId);
+            let sql1 = `SELECT * FROM role WHERE department_id = ${depId}`;
             let query1 = db.query(sql1, (err, results1) => {
                 if (err) throw err;
-                let role_id = results1[0].role_id;
-                let sql2 = `SELECT * FROM employees WHERE role_id = ${role_id}`;
+                // console.log(results1);
+                let roleId = results1[0].role_id;
+                // console.log(roleId);
+                let sql2 = `SELECT * FROM employees WHERE role_id = ${roleId}`;
                 let query2 = db.query(sql2, (err, results2) => {
                     if (err) throw err;
-
                     console.table(results2);
-                }
-                )
-            })
-        }
-        );
+                });
+            });
+        });
+    });
+}
 
-    }
-
-
-       
-};
-
-viewAllEmployeesByDepartment("hardware");
